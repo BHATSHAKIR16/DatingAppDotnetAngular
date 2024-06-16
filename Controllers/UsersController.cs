cd368054a1afc1cf030f228700cce2688ef963a2
Shakir Shafiq Bhat <shakibhat@deloitte.com>
2024-06-16 13:45:56 +0530

    first commit

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,33 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")] //api/Users
+    public class UsersController : ControllerBase
+    {
+        //create this field so that datacontext is available throughtout the class and not only in the constructor
+        private readonly DataContext _context;
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> getUsers()
+        {
+            var users = await _context.Users.ToListAsync();
+            return Ok(users);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> getUser(int id) 
+        {
+            var user = await _context.Users.FindAsync(id);
+            return Ok(user);
+        }
+    }
+}
